3a97003bcfecdac0cb3394039e12635e6abb66ab
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-06-24 11:08:21 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { App } from 'components/App';
-import './index.css';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
+const element = React.createElement('div', { a: 5 }, 'Текст');
+ReactDOM.render(element, document.getElementById('#root'));
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>,
-//   'Row'
+//   document.getElementById('#root')
 // );
 
-const element = <h1>Hello, world</h1>;
-ReactDOM.render(element, document.getElementById('#root'));
+// const element = <h1>Hello, world</h1>;
+// ReactDOM.render(element, document.getElementById('#root'));
